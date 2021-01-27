35e29ec063efe86b6925000da7ae936ebe0e14ae
Rickert Huismanius <70889425+99062653@users.noreply.github.com>
2021-01-27 15:39:28 +0100

    1.0

--- javascript/lingo.js
+++ javascript/lingo.js
@@ -3,20 +3,19 @@ var container = document.getElementById("SpeelVeld");
 var knop = document.getElementById("knop");
 knop.onclick = function() { 
     checkWoord(document.getElementById("Input").value);
-} 
+}
 var randomwoord;
 var attempts = 5;
-var attempt = 1;
+var attempt = 0;
 var lettersinput = [];
 var lettersrandom = [];
 
 function maakBord(){
-    for(var i = 1; i <= 25; i++){
-        var blok = document.createElement("div");
-        //blok.id = "W" + attempt + "L" + i;
-        container.appendChild(blok);
-        for(var r = 5; r <= 25; r++){
-            blok.id = "W" + r + "L" + i;
+    for(var l = 1; l <= 5; l++){
+        for(var i = 1; i <= 5; i++){
+            var blok = document.createElement("div");
+            blok.id = "W" + l + "L" + i;
+            container.appendChild(blok);   
         }
     }
 }
@@ -27,7 +26,7 @@ function randomWoord(){
 }
 
 function eersteLetter(){
-    var blok1 = document.getElementById("W1L5");
+    var blok1 = document.getElementById("W1L1" );
     blok1.innerHTML = "<h1></h1>" + randomwoord[0];
 }
 
@@ -41,12 +40,28 @@ function woordPlaatsen(woord){
     for(var i = 0; i < randomwoord.length; i++){
         lettersrandom[i] = randomwoord[i];
     }
+    //plaats de woordjes
+        for(var i = 0; i < lettersinput.length; i++){
+            document.getElementById("W"+ attempt +"L"+ (i+1)).innerHTML = "<h1></h1>" + lettersinput[i];
+
+            if(lettersrandom[i] == lettersinput[i]){
+                document.getElementById("W"+ attempt +"L" + (i+1)).style.backgroundColor = "green";
+                document.getElementById("W"+ (attempt + 1) + "L" + (i+1)).innerHTML
+                
+            }else{
+                if(randomwoord.includes(lettersinput[i])){
+                    document.getElementById("W"+ attempt +"L"+(i+1)).style.borderRadius = "50%";
+                    document.getElementById("W"+ attempt +"L"+(i+1)).style.backgroundColor = "yellow";
+            }
+        
+        }
+    }
 }
 
 function checkWoord(woord){
     attempt++
     woordPlaatsen(woord);
-    if(attempt > attempts){
+    if(attempt >= attempts){
         alert("Helaas, je hebt verloren.");
         location.reload();
     }
@@ -54,7 +69,7 @@ function checkWoord(woord){
         alert("Je hebt gewonnen!");
         location.reload();
     }
-    //var input = document.getElementById("Input").value;
+    console.log(attempt);
 }
 
 maakBord();
