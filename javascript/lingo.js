6079a0055ddda37946942789cb0b2b3a77ade070
Rickert Huismanius <70889425+99062653@users.noreply.github.com>
2021-01-27 11:17:42 +0100

    0.7

--- javascript/lingo.js
+++ javascript/lingo.js
@@ -1,33 +1,62 @@
-import {words} from "./lingo-nl.js"
-var container = document.getElementById("SpeelVeld");
-var submit = document.getElementById("submit");
-submit.onclick = InputValue;
+import { words } from "../javascript/lingo-nl.js"
+var container = document.getElementById("SpeelVeld"); 
+var knop = document.getElementById("knop");
+knop.onclick = function() { 
+    checkWoord(document.getElementById("Input").value);
+} 
+var randomwoord;
+var attempts = 5;
+var attempt = 1;
+var lettersinput = [];
+var lettersrandom = [];
 
-for(var i = 1; i <= 25; i++){
-    var blok = document.createElement("div");
-    container.appendChild(blok);
-    blok.id = "Blok_" + i;
-}
-
-function InputValue(){
-    var input = document.getElementById("input").value;
-
-    if(input.length > 5){
-        alert("Je input is te lang. Maximaal 5 letters.");
+function maakBord(){
+    for(var i = 1; i <= 25; i++){
+        var blok = document.createElement("div");
+        //blok.id = "W" + attempt + "L" + i;
+        container.appendChild(blok);
+        for(var r = 5; r <= 25; r++){
+            blok.id = "W" + r + "L" + i;
+        }
     }
 }
 
 function randomWoord(){
-    var randomwoord = words[Math.floor(Math.random() * words.length)];
+    randomwoord = words[Math.floor(Math.random() * words.length)];
     console.log(randomwoord);
-
-    var woordstuk = randomwoord.split;
-    console.log(woordstuk);
 }
 
-function woordRaden(){
-    var hint = document.getElementById("Blok_1");
-
+function eersteLetter(){
+    var blok1 = document.getElementById("W1L5");
+    blok1.innerHTML = "<h1></h1>" + randomwoord[0];
 }
 
+function woordPlaatsen(woord){
+     lettersinput = [];
+     lettersrandom = [];
+    
+    for (var i = 0; i < woord.length; i++) {
+        lettersinput[i] = woord[i];
+    }   
+    for(var i = 0; i < randomwoord.length; i++){
+        lettersrandom[i] = randomwoord[i];
+    }
+}
+
+function checkWoord(woord){
+    attempt++
+    woordPlaatsen(woord);
+    if(attempt > attempts){
+        alert("Helaas, je hebt verloren.");
+        location.reload();
+    }
+    if(woord == randomwoord){
+        alert("Je hebt gewonnen!");
+        location.reload();
+    }
+    //var input = document.getElementById("Input").value;
+}
+
+maakBord();
 randomWoord();
+eersteLetter();
